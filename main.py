b636946133222f43eab579af10837c873ff7fe7a
victorwilva <victorwilva@gmail.com>
2023-07-12 22:07:30 -0300

    Remover configuração importada de page.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import flet as ft
 
-import config.page
 import config.routes
 
 from componentes.NavigationBar import navigation_bar
@@ -9,9 +8,8 @@ from componentes.NavigationBar import navigation_bar
 def main(page: ft.Page):
 
     navigation_bar.page = page
-
     config.routes.RouteConfig(page)
-    config.page.PageConfig(page)
+   
     # firebase = config.firebase.FirebaseConfig(page)
 
 
