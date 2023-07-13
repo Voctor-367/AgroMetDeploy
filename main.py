061382fe83a3b8618950cf4a5a6cd8bb2e718180
victorwilva <victorwilva@gmail.com>
2023-07-12 22:32:13 -0300

    Ajustar parametro de visualizaçã do app na web

--- main.py
+++ main.py
@@ -13,4 +13,4 @@ def main(page: ft.Page):
     # firebase = config.firebase.FirebaseConfig(page)
 
 
-ft.app(main, view=ft.AppView.WEB_BROWSER, assets_dir="assets")
+ft.app(main, view=ft.WEB_BROWSER, assets_dir="assets")
